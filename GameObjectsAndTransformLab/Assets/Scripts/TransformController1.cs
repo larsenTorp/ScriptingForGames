43a1b2ebec19dc7093b807c6732c1d9672a13ca3
Larsen <10997557@uvu.edu>
2025-02-06 00:12:03 -0700

    Lab 2 Challenge changes
    Added a new variable r that increases over time to control rotations, also adjusted some variable values for more complexity

--- GameObjectsAndTransformLab/Assets/Scripts/TransformController1.cs
+++ GameObjectsAndTransformLab/Assets/Scripts/TransformController1.cs
@@ -7,10 +7,12 @@ public class TransformController : MonoBehaviour
     {
         // Move the target GameObject
         var x = Mathf.PingPong(t: Time.time, length: 3);
-        var p = new Vector3(x: 0, y: x, z: 0);
-            transform.position = p;
+        var r = Mathf.PingPong(t: Time.time, length: 9);
+        var p = new Vector3(x: x, y: x, z: 0);
+        transform.position = p;
+ 
 
         // Rotate target GameObject
-        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, r*10, r*10) * Time.deltaTime);
     }
 }
