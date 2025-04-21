d4c330addaf64d253382208581291fc0b90d7989
Larsen <10997557@uvu.edu>
2025-04-21 04:34:19 -0600

    Lab 7 Fix
    added a new line in HealthContainer that sets HealthData to 1 on game start

--- LabTesting/Assets/Scripts/HealthContainer.cs
+++ LabTesting/Assets/Scripts/HealthContainer.cs
@@ -4,6 +4,11 @@ public class HealthContainer : MonoBehaviour
 {
     public SimpleFloatData healthData;
 
+    private void Start()
+    {
+        healthData.SetValue(1);
+    }
+
     public void reduceHealth(float amount)
     { 
         healthData.UpdateValue(amount);
